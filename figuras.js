cba16996c735ff462dc3695a605e07c40c6d0cdc
lauradelarivera <lauradelarivera@gmail.com>
2021-09-30 22:22:18 +0200

    formulas and functions added on HTML

--- figuras.js
+++ figuras.js
@@ -103,6 +103,15 @@ function triangleAerea(baseT,heightT){
 }
 triangleAerea();
 
+//Isosceles Triangle
+
+function isoscelesH(sideT1, sideT2, baseT){
+    if (sideT1 != sideT2) {
+        console.error("The sides are not equal, this is not an Isosceles Triangle")
+    }
+    return (Math.sqrt((sideT1 * sideT2) - ((baseT * baseT)/4)))
+}
+isoscelesH();
 
 // Circle
 
@@ -121,6 +130,8 @@ circleAerea();
 
 //Working with HTML
 
+//Square
+
 function calculatePerimeterCuadrado(){
     const input = document.getElementById("InputSquare");
     const value = input.value;
@@ -138,3 +149,76 @@ function calculateAereaCuadrado(){
 }
 
 
+//Triangle
+
+function calculatePerimeterTriangle(){
+
+    const inputS1 = document.getElementById("InputTriangleS1");
+    const valueS1 = parseInt(inputS1.value);
+
+    const inputS2 = document.getElementById("InputTriangleS2");
+    const valueS2   = parseInt(inputS2.value);
+
+    const inputB3 = document.getElementById("InputTriangleB3");
+    const valueB3 = parseInt(inputB3.value);
+    
+    const perimeterT = perimeterTriangle(valueS1,valueS2, valueB3);
+    alert(perimeterT);
+}
+
+
+function calculateAereaTriangle(){
+
+    const inputB32 = document.getElementById("InputTriangleB3");
+    const valueB32 = parseInt(inputB32.value);
+
+    const inputH4 = document.getElementById("InputTriangleH4");
+    const valueH4 = parseInt(inputH4.value);
+    
+    const aereaT = triangleAerea(valueB32, valueH4);
+    alert(aereaT);
+}
+
+
+//Circle
+
+function calculatePerimeterCircle(){
+    const inputC = document.getElementById("InputCircle");
+    const valueC = inputC.value;
+
+    const perimeterC = diameterCircle(valueC);
+    alert(perimeterC);
+}
+
+function calculateAereaCircle(){
+    const inputC = document.getElementById("InputCircle");
+    const valueC = inputC.value;
+    
+    const aereaC = circleAerea(valueC);
+    alert(aereaC);
+
+}
+
+
+//Isosceles Triangle
+
+function calculateHeightIso() {
+    const inputIsoA = document.getElementById("InputIsoscelesA");
+    const valueA    = parseInt(inputIsoA.value);
+
+    const inputIsoB =  document.getElementById("InputIsoscelesB");
+    const valueB    =  parseInt(inputIsoB.value);
+
+    const inputBase =  document.getElementById("InputIsoscelesBase");
+    const valueBase = parseInt(inputBase.value);
+
+    const heightIs  = isoscelesH(valueA, valueB, valueBase);
+    
+    if     
+        (valueA != valueB) {
+        alert("The sides are not equal, this is not an Isosceles Triangle");
+        }   
+    else {
+            (alert(heightIs));
+        };            
+}
