883b0f224b3f004629a438dc91015ac2a984a747
lauradelarivera <lauradelarivera@gmail.com>
2021-10-04 02:31:32 +0200

    percentages, discounts and coupons

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 ...
 
 ## Workshop #1 - Geometric Figures
-
 - Define formulas
 - Implement formulas on JS
 - Create functions
 - Integrate JS to HTML
  
+
+ ## Workshop #2 - Percentage and discounts
+ - Define formulas
+ - Implement formulas on JS
+ - Create functions
+ - Integrate JS to HTML
---
+++ descuentos.js
@@ -0,0 +1,105 @@
+
+//PTP means "Price to Pay"
+
+const originalPrice = 100
+const discountP     = 15
+
+function ptp (price, discount){
+    return ("S/. " + (price*(100-discount)/100));
+}
+ptp();
+
+function priceDiscount(){
+    const inputPrice    = document.getElementById("inputPrice");
+    const valueP        = parseInt(inputPrice.value);
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const valueD        = parseInt(inputDiscount.value);
+    
+    const finalP        = ptp(valueP,valueD);
+
+    const finalPri        = document.getElementById("finalPri");
+    finalPri.innerText    = "The total ammount to pay is: "+ finalP; 
+}
+
+//Cupones
+
+const realPriceP = 100;
+const discountCN = 20;
+
+function ptpC (priceR, discountCC){
+    return ("S/. " +(priceR*(100-discountCC)/100));
+}
+
+
+
+function priceAfterCoupon(){
+    const firstPrice = document.getElementById("firstPrice");
+    const fpValue    = parseInt(firstPrice.value);
+    
+
+    const inputCupon = document.getElementById("inputCupon");
+    const cuponValue = inputCupon.value;
+
+    
+    if (cuponValue === "arbol"){
+        cuponDesc = 15;
+        const precioConDescuento = ptpC(fpValue,cuponDesc);
+        const finalCouponPrice = document.getElementById("finalCouponPrice");
+        finalCouponPrice.innerText = "The total ammount to pay is: " + precioConDescuento;
+
+    } else if (cuponValue === "casa"){
+        cuponDesc = 20;
+        const precioConDescuento = ptpC(fpValue,cuponDesc);
+        const finalCouponPrice = document.getElementById("finalCouponPrice");
+        finalCouponPrice.innerText = "The total ammount to pay is: " + precioConDescuento;
+        
+    } else if (cuponValue === "vaso"){
+        cuponDesc = 25;
+        const precioConDescuento = ptpC(fpValue,cuponDesc);
+        const finalCouponPrice = document.getElementById("finalCouponPrice");
+        finalCouponPrice.innerText = "The total ammount to pay is: " + precioConDescuento;
+    } else if (cuponValue === "crema"){
+        cuponDesc = 30;
+        const precioConDescuento = ptpC(fpValue,cuponDesc);
+        const finalCouponPrice = document.getElementById("finalCouponPrice");
+        finalCouponPrice.innerText = "The total ammount to pay is: " + precioConDescuento;
+    } else {
+        console.log("Not valid value :( ");
+    }
+
+    
+
+     
+
+
+}
+              
+
+
+
+//var cuponDesc;
+
+//switch(cuponValue){
+    //case coupons[0]:
+        //cuponDesc = 15;
+    //break;
+
+    //case coupons[1]:
+        //cuponDesc = 20;
+    //break;
+
+    //case coupons[2]:
+        //cuponDesc = 25
+    //break;
+
+    //case coupons[3]:
+        //cuponDesc = 30
+    //break;
+
+
+
+
+
+
+
